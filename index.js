baaa8a079c34d08c26f7c6ffd90c74a1409fdc38
Denis Krivolapov <it.technolo9y@yandex.ru>
2023-03-23 14:02:33 +0300

    23032023

--- index.js
+++ index.js
@@ -1,73 +1,30 @@
-const profileEditBottom = document.querySelector('.profile__editbottom');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close-button');
 const popupProfile = document.querySelector('.popup_type_profile');
-const popupClose = document.querySelector('.popup__close');
-const profileName = document.querySelector('.profile__name');
-const profileText = document.querySelector('.profile__text');
-const popup = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('#popup__name');
-let dataInput = formElement.querySelector('#popup__data');
+const nameProfile = document.querySelector('.profile__title');
+const jobProfile = document.querySelector('.profile__subtitle');
+let formElement = document.querySelector('.form');
+let nameInput = formElement.querySelector('#profile-name');
+let jobInput = formElement.querySelector('#profile-job');
 
-function openPopup() {
-    popup.classList.add('popup_open');
+function openPopup(popupName) {
+  popupName.classList.add('popup_opened');
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+}
+function closePopup(popupName) {
+  popupName.classList.remove('popup_opened');
+}
+
+closeButton.addEventListener('click', function () { closePopup(popupProfile); });
+editButton.addEventListener('click', function () { openPopup(popupProfile); });
+
+formElement.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  console.log(nameInput.value);
+  console.log(jobInput.value);
   
-  }
-  profileEditBottom.addEventListener('click', openPopup);
-
-
-
-
-
-
-
-
-
-
-  /*
-  // функция закрыть попап
-  function closePopup(popupName) {
-    popupName.classList.remove('.popup_open');
-  }
-  
-  // ===слушатели===
-  // закрыть попапы
-  popupClose.addEventListener('click', function () { closePopup(popupProfile); });
-  
-  // открыть попапы
-  profileEditBottom.addEventListener('click', function () { openPopup(popupProfile); });
-  
-  // submit - заглушка
-  formElement.addEventListener('submit', (e) => {
-    e.preventDefault();
-  
-    console.log(nameInput.value);
-    console.log(dataInput.value);
-    
-    closePopup(popupProfile);
-  });
-  
-
-
-
-
-/*profileEditBottom.addEventListener('click', function oponPopup(){
-    popup.classList.add('.popup_open')
+  closePopup(popupProfile);
 });
 
-/*profileEditBottom.addEventListener('click', () => {
-    popup.classList.add('.popup_open');
-});*/
-
-
-
-
-/*const openPopup = (popup) => {
-    popup.classList.add('.popup_open');
-}
-const closePopup = (popup) => {
-    popup.classList.remove('.popup_open');
-}
-
-
-profileEditBottom.addEventListener('click', () => openPopup(popup));
-popupClose.addEventListener('click', () => closePopup(popup));*/
